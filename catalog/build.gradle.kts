021e307f91d7e4950f2dfc76982614a7319957d3
buivietnhat <buivietnhatctqb@gmail.com>
2024-04-07 15:30:08 +0700

    add RockDB configuration and Arrow dependency

--- catalog/build.gradle.kts
+++ catalog/build.gradle.kts
@@ -1,26 +1,31 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
+    java
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.xodb"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.projectlombok:lombok")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	"developmentOnly"("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.projectlombok:lombok")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    "developmentOnly"("org.springframework.boot:spring-boot-devtools")
+
+    implementation("org.rocksdb:rocksdbjni:9.0.0")
+
+    implementation("org.apache.arrow:arrow-vector:9.0.0")
+    implementation("org.apache.arrow:arrow-memory-netty:9.0.0")
 
 //	implementation("org.apache.arrow:arrow-vector:5.0.0")
 //	implementation("org.apache.arrow:arrow-memory:5.0.0")
@@ -32,10 +37,10 @@ dependencies {
 //	implementation("org.apache.arrow:arrow-jdbc:5.0.0")
 //	implementation("org.apache.arrow:arrow-parquet:5.0.0")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- catalog/src/main/java/com/xodb/catalog/repository/impl/TableInfoRepositoryImpl.java
+++ catalog/src/main/java/com/xodb/catalog/repository/impl/TableInfoRepositoryImpl.java
@@ -2,8 +2,34 @@ package com.xodb.catalog.repository.impl;
 
 import com.xodb.catalog.dto.TableInfoDto;
 import com.xodb.catalog.repository.TableInfoRepository;
+import jakarta.annotation.PostConstruct;
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Value;
 
+@Repository
 public class TableInfoRepositoryImpl implements TableInfoRepository {
+  @Value("${db.rockdb.path}")
+  private String rockDBPath;
+
+  @PostConstruct
+  public void loadRockDb() {
+    RocksDB.loadLibrary();
+
+    try (final Options options = new Options().setCreateIfMissing(true)) {
+
+      // a factory method that returns a RocksDB instance
+      try (final RocksDB db = RocksDB.open(options, rockDBPath)) {
+
+        // do something
+      }
+    } catch (RocksDBException e) {
+      // do some error handling
+    }
+  }
+
   @Override
   public void saveOrUpdate(TableInfoDto tableInfoDto) {
 
