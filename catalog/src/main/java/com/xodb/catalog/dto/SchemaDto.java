0fe94b6ccf50c734e82afc7ca71366cc518e76fe
buivietnhat <buivietnhatctqb@gmail.com>
2024-04-18 10:35:43 +0700

    add TableInfoRepositoryImp test

--- catalog/src/main/java/com/xodb/catalog/dto/SchemaDto.java
+++ catalog/src/main/java/com/xodb/catalog/dto/SchemaDto.java
@@ -1,4 +1,8 @@
 package com.xodb.catalog.dto;
 
+import lombok.Data;
+
+@Data
 public class SchemaDto {
+  String jsonSchema;
 }
--- catalog/src/main/java/com/xodb/catalog/repository/impl/TableInfoRepositoryImpl.java
+++ catalog/src/main/java/com/xodb/catalog/repository/impl/TableInfoRepositoryImpl.java
@@ -1,11 +1,11 @@
 package com.xodb.catalog.repository.impl;
 
-import com.xodb.catalog.TableInfo;
-import com.xodb.catalog.XodbCatalogProtos;
 import com.xodb.catalog.dto.TableInfoDto;
 import com.xodb.catalog.exception.DBException;
+import com.xodb.catalog.exception.ResourceNotFoundException;
 import com.xodb.catalog.repository.TableInfoRepository;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.rocksdb.Options;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
@@ -18,18 +18,28 @@ public class TableInfoRepositoryImpl implements TableInfoRepository {
   private String rockDBPath;
 
   private RocksDB db;
+  private Options options;
 
   @PostConstruct
-  public void loadRockDb() {
+  private void initialize() {
     RocksDB.loadLibrary();
-
-    try (final Options options = new Options().setCreateIfMissing(true)) {
-      // a factory method that returns a RocksDB instance
-      db = RocksDB.open(options, rockDBPath);
+    options = new Options().setCreateIfMissing(true);
+    try {
+      String dataPath = this.getClass().getClassLoader().getResource("").getPath() + rockDBPath;
+      db = RocksDB.open(options, dataPath);
     } catch (RocksDBException e) {
-      // do some error handling
-      db.close();
       throw new DBException("cannot open RockDB with path " + rockDBPath + " + with error " + e.getMessage());
+    }
+  }
+
+  @PreDestroy
+  private void cleanUp() {
+    if (db != null) {
+      db.close();
+    }
+
+    if (options != null) {
+      options.close();
     }
   }
 
